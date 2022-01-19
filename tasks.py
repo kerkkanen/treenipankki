0e9705e2824321c6825b9057ba0d61b7b84c0af2
Katja Kerkkänen <katja.kerkkanen@helsinki.fi>
2022-01-19 10:36:18 +0200

    initializing

---
+++ tasks.py
@@ -0,0 +1,25 @@
+from invoke import task
+
+@task
+def  start(ctx):
+    ctx.run("python3 src/main.py")
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest")
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html")
+
+@task
+def test(ctx):
+    ctx.run("pytest src")
+
+@task
+def lint(ctx):
+    ctx.run("pylint src")
+
+@task
+def format(ctx):
+    ctx.run("autopep8 --in place --recursive src")
