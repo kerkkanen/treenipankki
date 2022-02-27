de0caddd6ab5b4104b6103c789470b73989dba70
kerkkanen <katjaker@cubbli20-gold.local>
2022-02-27 20:16:18 +0200

    small updates in appearance

--- sets.py
+++ sets.py
@@ -101,34 +101,6 @@ def delete_favourite(user_id, set_id):
     db.session.execute(sql, {"user_id": user_id, "set_id": set_id})
     db.session.commit()
 
-
-def get_reviews(set_id):
-    sql = """SELECT U.name, R.trainer_level, R.dumbells, R.comment FROM reviews R, users U WHERE R.creator_id = U.id AND R.set_id=:set_id"""
-    return db.session.execute(sql, {"set_id": set_id}).fetchall()
-
-def get_all_reviews():
-    sql = """SELECT * FROM reviews"""
-    return db.session.execute(sql).fetchall() 
-
-
-def get_review_average(set_id):
-    sql = """SELECT AVG(dumbells) FROM reviews WHERE set_id=:set_id"""
-    return db.session.execute(sql, {"set_id": set_id}).fetchone()
-
-
-def get_review_volume(set_id):
-    sql = """SELECT COUNT(set_id) FROM reviews WHERE set_id=:set_id"""
-    return db.session.execute(sql, {"set_id": set_id}).fetchone()
-
-
-def add_review(creator_id, set_id, trainer_level, dumbells, comment):
-    sql = """INSERT INTO reviews (creator_id, set_id, trainer_level, dumbells, comment)
-             VALUES (:creator_id, :set_id, :trainer_level, :dumbells, :comment)"""
-    db.session.execute(
-        sql, {"creator_id": creator_id, "set_id": set_id, "trainer_level": trainer_level, "dumbells": dumbells, "comment": comment})
-    db.session.commit()
-
-
 def delete(set_id):
     sql = """DELETE FROM sets WHERE sets.id=:set_id"""
     db.session.execute(sql, {"set_id": set_id})
